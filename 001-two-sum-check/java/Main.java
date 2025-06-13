20d24a439cc4ce19f46120ae8f4bf9f9048a87f8
Paweł Pokorski <pawelpokorski@interia.eu>
2025-06-13 22:34:23 +0200

    Update Main.java

--- 001-two-sum-check/java/Main.java
+++ 001-two-sum-check/java/Main.java
@@ -1,1 +1,35 @@
+import java.util.HashSet;
+import java.util.Scanner;
 
+public class App {
+
+    static Integer[] numbers = {10, 15, 3, 7};
+
+    public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number k: ");
+        
+        if (scanner.hasNextInt()) {
+            Integer k = scanner.nextInt();
+            System.out.println(checkSum(k));
+        } else {
+            System.out.println("Invalid input.");
+        }
+
+        scanner.close();
+    }
+
+    static boolean checkSum(Integer k) {
+        HashSet<Integer> seenNumbers = new HashSet<>();
+
+        for (Integer number : numbers) {
+            int complement = k - number;
+            if (seenNumbers.contains(complement)) {
+                return true;
+            }
+            seenNumbers.add(number);
+        }
+
+        return false;
+    }
+}
