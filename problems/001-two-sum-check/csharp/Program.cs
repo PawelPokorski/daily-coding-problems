95c5d14b21eea3ee2a2d64206329ff49b6112760
Paweł Pokorski <pawelpokorski@interia.eu>
2025-06-14 11:25:40 +0200

    Update Program.cs

--- problems/001-two-sum-check/csharp/Program.cs
+++ problems/001-two-sum-check/csharp/Program.cs
@@ -1,1 +1,33 @@
+using System;
+using System.Collections.Generic;
 
+public class Program
+{
+    static int[] numbers = [10, 15, 3, 7];
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number k: ");
+
+        if (int.TryParse(Console.ReadLine(), out int k))
+            Console.WriteLine(CheckSum(k));
+        else
+            Console.WriteLine("Invalid input.");
+    }
+
+    static bool CheckSum(int k)
+    {
+        HashSet<int> seenNumbers = [];
+
+        foreach (int number in numbers)
+        {
+            int complement = k - number;
+            if (seenNumbers.Contains(complement))
+                return true;
+
+            seenNumbers.Add(number);
+        }
+
+        return false;
+    }
+}
