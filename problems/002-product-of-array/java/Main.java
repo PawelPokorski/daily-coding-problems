19663df29e2875a0a35e3b08f9b6b000f2bdd333
Paweł Pokorski <pawelpokorski@interia.eu>
2025-06-15 13:35:21 +0200

    Update Main.java

--- problems/002-product-of-array/java/Main.java
+++ problems/002-product-of-array/java/Main.java
@@ -1,1 +1,29 @@
+import java.util.Arrays;
 
+public class App {
+
+    static Integer[] numbers = {1, 2, 3, 4, 5};
+
+    public static void main(String... args) {
+
+        var output = arrayProduct(numbers);
+
+        System.out.println(Arrays.toString(output));
+    }
+
+    static Integer[] arrayProduct(Integer[] array) {
+        int length = array.length;
+        Integer[] productArray = new Integer[length];
+        Arrays.fill(productArray, 1);
+
+        for(int i = 0; i < length; i++) {
+            for(int j = 0; j < length; j++) {
+                if(i != j) {
+                    productArray[i] *= array[j];
+                }
+            }
+        }
+
+        return productArray;
+    }
+}
