49f65a2509ded1adaaaa6b5b9918b78c57b06eb1
Paweł Pokorski <pawelpokorski@interia.eu>
2025-06-15 13:25:06 +0200

    Update Program.cs

--- problems/002-product-of-array/csharp/Program.cs
+++ problems/002-product-of-array/csharp/Program.cs
@@ -1,1 +1,34 @@
 
+using System;
+using System.Linq;
+
+public class Program
+{
+    static int[] numbers = [1, 2, 3, 4, 5];
+
+    static void Main(string[] args)
+    {
+        var output = ArrayProduct(numbers);
+
+        foreach (var o in output)
+        {
+            Console.Write($"{o} ");
+        }
+    }
+
+    static int[] ArrayProduct(int[] array)
+    {
+        int[] productArray = [.. Enumerable.Repeat(1, array.Length)];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (i != j)
+                    productArray[i] *= array[j];
+            }
+        }
+
+        return productArray;
+    }
+}
