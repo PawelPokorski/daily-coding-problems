3bd9fb98661728fa59923cb1df0fa822ca5cee89
Paweł Pokorski <pawelpokorski@interia.eu>
2025-06-19 19:08:21 +0200

    Update Program.cs

--- problems/005-functional-pair-implementation/csharp/Program.cs
+++ problems/005-functional-pair-implementation/csharp/Program.cs
@@ -1,1 +1,30 @@
+using System;
 
+class Program
+{
+    public delegate object Pair(Func<object, object, object> f);
+
+    public static Pair Cons(object a, object b)
+    {
+        Pair pair = f => f(a, b);
+        return pair;
+    }
+
+    public static object Car(Pair pair)
+    {
+        return pair((a, b) => a);
+    }
+
+    public static object Cdr(Pair pair)
+    {
+        return pair((a, b) => b);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Car(Cons(3, 4)));
+        Console.WriteLine(Cdr(Cons(3, 4)));
+        Console.WriteLine(Cdr(Cons(3, "string")));
+        Console.WriteLine(Car((Pair)Cdr(Cons("first", Cons("second", "third")))));
+    }
+}
